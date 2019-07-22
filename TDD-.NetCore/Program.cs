eff5fbc859e859a4db4822124b8151a98d174092
Nicolas Fontes <nicolas.rfontes@outlook.com>
2019-07-22 08:29:32 +0100

    First Project Structure

---
+++ TDD-.NetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TDD_.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
